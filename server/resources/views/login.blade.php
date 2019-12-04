723d4a99ab08d39dcb601e702d0ebcf977efe799
中野雄貴 <totoro.windows@outlook.com>
2019-12-04 16:41:31 +0900

    callback URLの変更
    ルーティング設定

---
+++ server/resources/views/login.blade.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+    <a href="https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=1653607210&redirect_uri=https://misosiru.ml/callback.php&state=123abc">LINE Login</a>
+{{--    https://api.line.me/v1/oauth/accessToken?grant_type=tnIM4OBm2Y2njqqlYX8F&code=b5fd32eacc791df&client_id=1653607210&client_secret=a4d915c1d2b983f30a726f40370ba742&redirect_uri=https://misosiru.ml/callback.php--}}
+</body>
+</html>
+
+<?php
+?>
--- server/routes/web.php
+++ server/routes/web.php
@@ -14,3 +14,33 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+# # # # #
+#  ログイン系
+# # # # #
+// ログインページ
+Route::get('/login', function () {
+    return view('login');
+});
+
+// ログイン処理
+Route::post('/login', function () {
+});
+
+// ログアウト処理
+Route::get('/logout', function () {
+});
+
+# # # # #
+# OAuth
+# # # # #
+Route::post('/callback', function () {
+});
+
+# # # # #
+# マイページ
+# # # # #
+// マイページ
+Route::get('/myPage', function () {
+});
+
